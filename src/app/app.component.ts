395fc750bedfb5337016f280433681db63ce01b8
Ahmet Çetinkaya <ahmetcetinkaya@tutamail.com>
2024-04-15 11:02:09 +0300

    Giriş

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { SharedModule } from './shared/shared.module';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, SharedModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'etiya-fs-angular';
+
+  products = [
+    {
+      name: 'Samsung Galaxy S10',
+      price: 900,
+      discontinued: true,
+    },
+    {
+      name: 'Iphone 11',
+      price: 1200,
+      discontinued: false,
+    },
+    {
+      name: 'Huawei P30',
+      price: 800,
+      discontinued: false,
+    },
+  ];
 }
